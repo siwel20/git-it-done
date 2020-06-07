b5a9be1d7495055933767103dacf9cb3bd9b4238
siwel20 <plthrock@gmail.com>
2020-06-06 18:16:22 -0600

    added feature to search by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
@@ -16,6 +17,18 @@ var formSubmitHandler = function(event) {
     alert("Please enter a GitHub username");
   }
 };
+
+var buttonClickHandler = function(event) {
+  event.preventDefault();
+
+  var language = event.target.getAttribute("data-language"); 
+   if (language) {
+     getFeaturedRepos(language);
+
+     // clear old content
+     repoContainerEl.textContent = "";
+   }
+}
 
 var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
@@ -85,5 +98,19 @@ var getUserRepos = function(user) {
     alert("Unable to connect to GitHub");
   });
 };
-userFormEl.addEventListener("submit", formSubmitHandler);
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler); 
